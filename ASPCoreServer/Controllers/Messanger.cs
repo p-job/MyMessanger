ca029d280aed2f14d552bdb3df5f581efdeaafda
Павел <p-jobsearch@yandex.ru>
2021-07-19 02:57:16 +0300

    Добавление GET и POST запросов для сервера

--- ASPCoreServer/Controllers/Messanger.cs
+++ ASPCoreServer/Controllers/Messanger.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
+using MyMessanger;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,18 +12,31 @@ namespace ASPCoreServer.Controllers
     [ApiController]
     public class Messanger : ControllerBase
     {
-
+        static List<Message> ListOfMessages = new List<Message>();
         // GET api/<Messanger>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "Some value = " + id.ToString();
+            string outputString = "Not found";
+            if ((id < ListOfMessages.Count) && (id >= 0))
+            {
+                outputString = JsonConvert.SerializeObject(ListOfMessages[id]);
+            }
+            Console.WriteLine(String.Format("Запрошено сообщение № {0}: {1}", id, outputString));
+            return outputString;
         }
 
         // POST api/<Messanger>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult SendMessage([FromBody] Message msg)
         {
+            if (msg == null)
+            {
+                return BadRequest();
+            }
+            ListOfMessages.Add(msg);
+            Console.WriteLine(String.Format("Всего сообщений: {0} Посланное сообщение: {1}", ListOfMessages.Count, msg));
+            return new OkResult();
         }
 
     }
