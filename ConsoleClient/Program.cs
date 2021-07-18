f1bbd3c18d3f67ebc51f5d348acf9a9d9f42a708
Павел <p-jobsearch@yandex.ru>
2021-07-19 00:49:36 +0300

    Добавил класс Message

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,7 +6,9 @@ namespace MyMessanger
     {
         static void Main(string[] args)
         {
+            Message msg = new Message();
             Console.WriteLine("Начало проекта!");
+            Console.WriteLine(msg.ToString());
         }
     }
 }
