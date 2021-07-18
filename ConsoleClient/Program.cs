cc7609b9e96eea81f972fe496d5b3cab89b0b7c1
Павел <p-jobsearch@yandex.ru>
2021-07-19 00:11:51 +0300

    Перенесли проект в отдельную папку

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMessanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
