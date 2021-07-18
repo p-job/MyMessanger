09139856471a05224ee42620d3ce03a8178307b4
Павел <p-jobsearch@yandex.ru>
2021-07-19 01:04:35 +0300

    Добавлен пакет Newtonsoft и сериализация

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyMessanger
 {
@@ -6,9 +7,14 @@ namespace MyMessanger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Начало проекта!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("Paul", "Hello", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+
+            //{ "UserName":"Paul","MessageText":"Hello","TimeStamp":"2021-07-18T22:00:33.9904406Z"}
+            //Paul < 18.07.2021 22:00:33 >: Hello
         }
     }
 }
